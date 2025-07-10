4b222da57c393b2d31136d5e15facab731106988
Miranda Paris <miparis@c1r11s4.42madrid.com>
2025-07-10 13:08:34 +0200

    Notes & cleaning

--- src/raycaster/movement.c
+++ src/raycaster/movement.c
@@ -3,15 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   movement.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: saragar2 <saragar2@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: miparis <miparis@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 18:53:49 by saragar2          #+#    #+#             */
-/*   Updated: 2025/07/09 17:47:44 by saragar2         ###   ########.fr       */
+/*   Updated: 2025/07/10 13:00:41 by miparis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
+/*Actualizando las corrdenadas del jugador. 
+Se chequea las colisiones por celda (X e Y) y luego se actualiza			*/
 static int	update_position(t_data *data, double x, double y)
 {
 	double	new_x;
@@ -21,6 +23,7 @@ static int	update_position(t_data *data, double x, double y)
 	new_y = data->player->pos_y + y;
 	if (!touch(data, data->player->pos_y, new_x))
 		data->player->pos_x = new_x;
+	//esto es lo que permite que eñ jugador se deslize si hay una colision
 	if (!touch(data, new_y, data->player->pos_x))
 		data->player->pos_y = new_y;
 	return (0);
@@ -33,7 +36,8 @@ int	touch(t_data *data, int px_map_y, int py_map_x)
     // Primero, verifica que la fila (py_map_y) sea válida antes de intentar obtener su longitud.
 	if (px_map_y < 0 || px_map_y >= (int)data->map->height)
 	{
-		printf("collision, px_map_y: %i\n", px_map_y);
+		error_msg("\nError: Collision detected");
+		printf("px_map_y: %i\n", px_map_y);
 		return (1);
 	}
     // Comprueba los límites horizontales (py_map_x)
@@ -44,6 +48,7 @@ int	touch(t_data *data, int px_map_y, int py_map_x)
 	return (0);
 }
 
+/*Se encarga de chequear los moviminetos en movimiento y rotacion de vista. Calcula deltaX y deltaY (mediante sin() y coss())*/
 int	key_control(int keycode, t_data *data)
 {
 	if (keycode == 65307) //"ESC"
@@ -69,72 +74,108 @@ int	key_control(int keycode, t_data *data)
 	return (0);
 }
 
-float distance(float x, float y){
-    return sqrt(x * x + y * y);
+/*Calcula la distancia euclidiana (la distancia "en línea recta") desde el origen (0,0) hasta un punto (x, y) en un plano 2D.
+Obtiene la distancia entre dos puntos (el jugador y el punto de impacto del rayo), pasándole las diferencias de coordenadas delta_x y delta_y.*/
+float	distance(float x, float y)
+{
+	return (sqrt(x * x + y * y));
 }
 
-float fixed_dist(float x1, float y1, float x2, float y2, t_data *game)
+//Calcula la distancia perpendicular desde el jugador al rayo. Corrige ojo de pez
+float	fixed_dist(float x1, float y1, float x2, float y2, t_data *game)
 {
-    float delta_x = x2 - x1;
-    float delta_y = y2 - y1;
-   	float angle = atan2(delta_y, delta_x) - game->player->angle;
-    float fix_dist = distance(delta_x, delta_y) * cos(angle);
-    return fix_dist;
+	float	delta_x;
+	float	delta_y;
+	float	angle;
+	float	fix_dist;
+
+	delta_x = x2 - x1; //Posicion del jugador para calculo vector de direccion
+	delta_y = y2 - y1;//Punto de impacto para calculo vector de lonngitud	
+	/*Se calcula el ángulo absoluto del rayo con respecto al eje X. 
+	 atan2 devuelve el ángulo en radianes entre el eje X positivo y el vector (delta_x, delta_y). */
+	/*Se resta el ángulo absoluto del jugador game->player->angle al ángulo absoluto del rayo.
+	 Angle es el ángulo entre el rayo y la dirección de la vista del jugador.*/
+	angle = atan2(delta_y, delta_x) - game->player->angle;	
+	/*Multiplicar la distancia euclidiana por el coseno del ángulo relativo proyecta esa distancia sobre la dirección de la vista del jugador. */
+	fix_dist = distance(delta_x, delta_y) * cos(angle);
+	return (fix_dist);
 }
 
-static void draw_line(t_player *player, t_data *game, float start_x, int i)
+static void	draw_line(t_player *player, t_data *game, float start_x, int i)
 {
-    float cos_angle = cos(start_x);
+    float cos_angle = cos(start_x); //Se calculan segun el angulo del rayo en base a la pos actual
     float sin_angle = sin(start_x);
     float ray_x = player->pos_x;
     float ray_y = player->pos_y;
-	float fov_y = FOV * SCREEN_HEIGHT / SCREEN_WIDTH;
-	float proj_plane_height = 2 * tan(fov_y / 2);
+	float fov_y = FOV * SCREEN_HEIGHT / SCREEN_WIDTH; // Convierte las distancias del campo de vision en base al tamaño de pantalla
+	float proj_plane_height = 2 * tan(fov_y / 2); //Distancia del plano de proyeccion
 	
     while(!touch(game, ray_y, ray_x))
     {
+		//Aqui deberia ir el calculo del DDA. Se calcula la posicion desde donde se lanza el rayo
         ray_x += cos_angle * 0.02; //esto y lo de abajo es lo que se usa para suavizar los bordes/ojo de pez
         ray_y += sin_angle * 0.02;
     }
+	//Calcula la distancia perpendicular desde el jugador al rayo. Corrige ojo de pez
 	float dist = (fixed_dist(player->pos_x, player->pos_y, ray_x, ray_y, game));
+	//Calcula la altura de la columna de la pared segun la distancia (más pequeña o grande) y el plano de vision
 	float height = ((1 / dist) / (proj_plane_height) * SCREEN_HEIGHT);
 
-	float start_y = (SCREEN_HEIGHT - height) / 2;
-	float end = start_y + height;
-	while(start_y < end)
+	float start_y = (SCREEN_HEIGHT - height) / 2; //centrar columna
+	float end = start_y + height; //chequea que no nos vayamos de mapa
+	 
+	while(start_y < end) //dibuja paredes
 	{
-		put_pixel(game, i, start_y, 0xff0000);
+		put_pixel(game, i, start_y, 0xff0000); //Aqui tenemos que poner la asignacion de texutras segun el DDA
 		start_y++;
 	}
 }
 
 static void clear_image(t_data *game)
 {
-    for(int y = 0; y < SCREEN_HEIGHT; y++)
-        for(int x = 0; x < SCREEN_WIDTH; x++)
-            put_pixel(game, x, y, 0);
+	int	y;
+	int	x;
+
+	y = 0;
+	while (y < SCREEN_HEIGHT)
+	{
+		x = 0;
+		while (x < SCREEN_WIDTH)
+		{
+			put_pixel(game, x, y, 0);
+			x += 1;
+		}
+		y += 1;
+	}
+    //Normi is not happy with this loop
+	//for(int y = 0; y < SCREEN_HEIGHT; y++)
+    //    for(int x = 0; x < SCREEN_WIDTH; x++)
+    //        put_pixel(game, x, y, 0);
 }
+
 
 int draw_loop(t_data *game)
 {
     t_player *player = game->player;
 	
-	player->angle += player->angle_flag;
+	//Guardamos y modificamos directamente el vector del jugador
+	player->angle += player->angle_flag; //angulo absoluto del jugador o delta de rotacion
     clear_image(game);
-    float fraction = FOV / SCREEN_WIDTH;
-    float start_x = player->angle - FOV / 2;
+    float fraction = FOV / SCREEN_WIDTH; //Calcula el incremento angular para cada rayo (columna).
+    float start_x = player->angle - FOV / 2; //Ángulo del primer rayo (extremo izquierdo del FOV).
     int i = 0;
-    while(i < SCREEN_WIDTH)
+
+    while(i < SCREEN_WIDTH)//itera columna por columna pintandolo
     {
-		player->angle_flag = 0;
+		player->angle_flag = 0; 
         draw_line(player, game, start_x, i);
-        start_x += fraction;
+        start_x += fraction; //siguiente rayo ya tu sabe
         i++;
     }
 
 	if (game->mlx_ptr == NULL || game->w_ptr == NULL || game->img == NULL)
 	{
-		printf("\nError: Failed to allocate MLX, window or image\n");
+		error_msg("\nError: Failed to allocate MLX, window or image\n");
 		exit (1);
 	}
     return 0;
